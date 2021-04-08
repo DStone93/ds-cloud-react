d5531ba4cd43668ad72b84db22b73d08561783c9
Derrick <dstone16640@alumni.focuscollege.com>
2021-04-08 14:26:24 -0700

    changed website title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { Switch, Route } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import { HomePage } from "./Components/HomePage/HomePage";
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import {HomePage} from './Components/HomePage/HomePage'
 // import './style.scss';
 
 export default function DStone(props: {}) {
+
+
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={HomePage} />
       </Switch>
     </BrowserRouter>
-  );
+  )
 }
