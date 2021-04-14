ca78a616493f3819270093b5238fecbe1d79f8cb
Derrick <dstone16640@alumni.focuscollege.com>
2021-04-13 19:17:24 -0700

    Trying to get emailJs to work

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -3,6 +3,8 @@ import { MainLayout } from "../MainLayout/MainLayout";
 import { useState } from "react";
 import gitHubLogo from "../../data/gitHubLogo.png";
 import Derrick_Stone_Resume from "../../resume/Derrick_Stone_Resume.pdf";
+import {template_id, service_id, user_id} from '../../keys';
+import * as emailjs from 'emailjs-com';
 import "./style.scss";
 
 export function HomePage(props: {}) {
@@ -10,15 +12,38 @@ export function HomePage(props: {}) {
   const [usersEmail, setUsersEmail] = useState("");
   const [usersMessage, setUsersMessage] = useState("");
 
+  let data = {
+    from_name: usersName,
+    from_email: usersEmail,
+    message: usersMessage,
+
+  };
+
+  console.log(usersEmail, usersMessage, usersName)
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
 
-    let data = {
-      from_name: usersName,
-      from_email: usersEmail,
-      message: usersMessage,
-    };
+    emailjs.send(
+      service_id,
+      template_id,
+      data,
+      user_id
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
   };
+
+  function validateEmail (email:any){
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(!regex.test(email)){
+      return "Invalid E-mail"
+    }
+  }
 
   return (
     <MainLayout>
@@ -52,6 +77,8 @@ export function HomePage(props: {}) {
       </section>
 
       <section id="contactPage">
+      <p>Contact me at <a href="mailto:dstone16640@alumni.focuscollege.com">dstone16640@alumni.focuscollege.com</a></p>
+
         <div className="contactContainer">
           <Form>
             <Row className="RowSpace">
@@ -67,6 +94,8 @@ export function HomePage(props: {}) {
             </Row>
             <Row className="RowSpace">
               <Form.Control
+                // pattern='/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
+                type="email"
                 name="from_email"
                 onChange={(e) => setUsersEmail(e.target.value)}
                 placeholder="Enter E-mail"
@@ -80,7 +109,9 @@ export function HomePage(props: {}) {
                 placeholder="Enter Message"
               ></Form.Control>
             </Row>
-            <button onChange={handleSubmit} disabled>
+            <button
+            className="submitButton" 
+            onChange={handleSubmit}>
               {" "}
               Submit{" "}
             </button>
